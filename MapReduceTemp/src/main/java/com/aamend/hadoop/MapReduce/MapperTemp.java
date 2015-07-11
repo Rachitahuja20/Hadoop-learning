1784c6bb25b83e5de52163d0bbaf16a64987fa7f
Rachitahuja20 <rachitahuja20@gmail.com>
2015-07-11 10:44:43 +0100

    Changes

--- MapReduceTemp/src/main/java/com/aamend/hadoop/MapReduce/MapperTemp.java
+++ MapReduceTemp/src/main/java/com/aamend/hadoop/MapReduce/MapperTemp.java
@@ -1,5 +1,9 @@
 package com.aamend.hadoop.MapReduce;
 
+/*
+ * To find the Maximum temperature for the years from the weather data set.
+ * 
+ */
 import java.io.IOException;
 
 import org.apache.hadoop.io.IntWritable;
@@ -7,8 +11,12 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class MapperTemp extends
-    Mapper<LongWritable, Text, Text, IntWritable> {
+public class MapperTemp extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+  int yearIndex = 0;
+  int lengthIndex = 3;
+  int tempIndex = 1;
+  int qualityIndex = 2;
 
   private static final int Lost = 9999;
 
@@ -17,13 +25,13 @@ public class MapperTemp extends
 
     String Line = value.toString();
     String[] recordSplits = Line.split("\t");
-    String Year = recordSplits[0];
+    String Year = recordSplits[yearIndex];
 
     int airTemperature;
-    if (recordSplits.length == 3) {
-      airTemperature = Integer.parseInt(recordSplits[1]);
+    if (recordSplits.length == lengthIndex) {
+      airTemperature = Integer.parseInt(recordSplits[tempIndex]);
 
-      String quality = recordSplits[2];
+      String quality = recordSplits[qualityIndex];
       if (airTemperature != Lost && quality.matches("[01459]")) {
         context.write(new Text(Year), new IntWritable(airTemperature));
       }
--- MapReduceTemp/src/main/java/com/aamend/hadoop/MapReduce/ReducerTemp.java
+++ MapReduceTemp/src/main/java/com/aamend/hadoop/MapReduce/ReducerTemp.java
@@ -6,14 +6,14 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class ReducerTemp extends
-    Reducer<Text, IntWritable, Text, IntWritable> {
+public class ReducerTemp extends Reducer<Text, IntWritable, Text, IntWritable> {
 
   public void reduce(Text key, Iterable<IntWritable> values, Context context)
       throws IOException, InterruptedException {
 
     int maxValue = Integer.MIN_VALUE;
     for (IntWritable value : values) {
+      // To find the maximum temperature for the year provided to the reducer
       maxValue = Math.max(maxValue, value.get());
 
     }
